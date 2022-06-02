af4cea67022c132100faf1ec7924c2489b2ea39d
Дмитрий Мельков <92420054+DmitryMelkov@users.noreply.github.com>
2022-06-02 20:09:10 +0500

    add corrections

--- unix303.js
+++ unix303.js
@@ -1,54 +1,48 @@
 window.addEventListener("DOMContentLoaded", function () {
-  let a = 84.5;
-  let b = 46.4;
-  let c = 1.3;
-  let z = 58.38;
-  let a1 = a - c * 2;
-  let b1 = b - c * 2;
-  console.log(a1 + " Длина а1");
-  console.log(b1 + " Длина b1");
+
+
 
   // let x1 = parseInt(prompt('vvedite x1'));
   // let y1 = parseInt(prompt('vvedite y1'));
   // let z1 = parseInt(prompt('vvedite z1'));
   // let n1 = {x1, y1, z1};
 
-  let n1 = { x1: 135.29, y1: 31.99, z1: 99 };
-  console.log(n1);
+  // let n1 = { x1: 135.29, y1: 31.99, z1: 99 };
+  // console.log(n1);
 
-  let n2 = {};
-  n2.x2 = n1.x1;
-  n2.y2 = n1.y1;
-  n2.z2 = n1.z1 + z;
-  console.log(n2);
+  // let n2 = {};
+  // n2.x2 = n1.x1;
+  // n2.y2 = n1.y1;
+  // n2.z2 = n1.z1 + z;
+  // console.log(n2);
 
-  let n3 = {};
-  n3.x3 = n2.x2 - a1;
-  n3.y3 = n2.y2;
-  n3.z3 = n2.z2;
-  console.log(n3);
+  // let n3 = {};
+  // n3.x3 = n2.x2 - a1;
+  // n3.y3 = n2.y2;
+  // n3.z3 = n2.z2;
+  // console.log(n3);
 
-  let n4 = {};
-  n4.x4 = n3.x3;
-  n4.y4 = n3.y3 + b1;
-  n4.z4 = n3.z3;
-  console.log(n4);
+  // let n4 = {};
+  // n4.x4 = n3.x3;
+  // n4.y4 = n3.y3 + b1;
+  // n4.z4 = n3.z3;
+  // console.log(n4);
 
-  let n5 = {};
-  n5.x5 = n2.x2;
-  n5.y5 = n4.y4;
-  n5.z5 = n4.z4;
-  console.log(n5);
+  // let n5 = {};
+  // n5.x5 = n2.x2;
+  // n5.y5 = n4.y4;
+  // n5.z5 = n4.z4;
+  // console.log(n5);
 
-  let n6 = {};
-  n6.x6 = n2.x2;
-  n6.y6 = n2.y2;
-  n6.z6 = n4.z4;
-  console.log(n6);
+  // let n6 = {};
+  // n6.x6 = n2.x2;
+  // n6.y6 = n2.y2;
+  // n6.z6 = n4.z4;
+  // console.log(n6);
 
-  let n7 = {};
-  n7.z7 = n1.z1;
-  console.log(n7);
+  // let n7 = {};
+  // n7.z7 = n1.z1;
+  // console.log(n7);
 
   // let num = 1.23456;
 
@@ -60,6 +54,17 @@ window.addEventListener("DOMContentLoaded", function () {
   let buttonRun = document.querySelector("#ok-block");
 
   buttonRun.addEventListener("click", function () {
+    let a = document.querySelector("#a-input").value;
+    let b = document.querySelector("#b-input").value;
+
+    let c = 1.3;
+    let z = 58.38;
+    let a1 = a - c * 2;
+    let b1 = b - c * 2;
+    console.log(a1 + " Длина а1");
+    console.log(b1 + " Длина b1");
+
+
     x1Value = xInput.value;
     y1Value = yInput.value;
     z1Value = zInput.value;
